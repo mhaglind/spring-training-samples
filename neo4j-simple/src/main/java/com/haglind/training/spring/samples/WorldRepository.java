480263a4e1cbac5537ffa7691667ce08286b1637
unknown <mhaglin1@GOT100CTZ545J.vccnet.volvocars.net>
2013-04-16 11:19:03 +0200

    First commit

---
+++ neo4j-simple/src/main/java/com/haglind/training/spring/samples/WorldRepository.java
@@ -0,0 +1,5 @@
+package com.haglind.training.spring.samples;
+
+import org.springframework.data.neo4j.repository.GraphRepository;
+
+public interface WorldRepository extends GraphRepository<World> {}
